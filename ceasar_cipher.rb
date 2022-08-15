e13ecbc9b5f1bdc782ea1d52a0c01b5e66f18e60
Lucas Eduardo Campolino <campolino.l@gmail.com>
2022-08-15 20:08:14 -0300

    Ceasar Cipher is working

---
+++ ceasar_cipher.rb
@@ -0,0 +1,15 @@
+def ceasar_cipher(string, number, new_char = '')
+  string.chars do |char|
+    if char[/^[a-zA-Z_]+/]
+      base = char.ord < 91 ? 65 : 97
+
+      code_char = (((char.ord - base) + number) % 26) + base
+      new_char += code_char.chr
+    else
+      new_char += char
+    end
+  end
+  p new_char
+end
+
+ceasar_cipher("What a string!", 5)
