3ae1f1999433fdf71420f83ac7366290b7698ad6
Lucas Eduardo Campolino <campolino.l@gmail.com>
2022-08-15 20:14:55 -0300

    README.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ceaser-cipher
+
+Ceasar Cipher cryptography programm, where a text is encrypted by a random number
+
+# how to use
+
+Create a new object of Encrypt and call the method 'ceaser_cipher' with a text, of class string, and a number of times the character should be shifted
--- ceasar_cipher.rb
+++ ceasar_cipher.rb
@@ -1,15 +1,18 @@
-def ceasar_cipher(string, number, new_char = '')
-  string.chars do |char|
-    if char[/^[a-zA-Z_]+/]
-      base = char.ord < 91 ? 65 : 97
-
-      code_char = (((char.ord - base) + number) % 26) + base
-      new_char += code_char.chr
-    else
-      new_char += char
+class Encrypt
+  def ceasar_cipher(string, number, new_char = '')
+    string.chars do |char|
+      if char[/^[a-zA-Z_]+/]
+        base = char.ord < 91 ? 65 : 97
+  
+        code_char = (((char.ord - base) + number) % 26) + base
+        new_char += code_char.chr
+      else
+        new_char += char
+      end
     end
+    p new_char
   end
-  p new_char
 end
 
-ceasar_cipher("What a string!", 5)
+encrypt = Encrypt.new
+encrypt.ceasar_cipher("What a string!", 5)
